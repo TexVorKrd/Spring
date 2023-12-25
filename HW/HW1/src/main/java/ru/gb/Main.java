0561288ef4ffd33d069730db3774c0d6faaddfac
Sergey Vorotilin <sflash@mail.ru>
2023-12-26 00:31:22 +0300

    HW1 MVN

---
+++ HW/HW1/src/main/java/ru/gb/Main.java
@@ -0,0 +1,31 @@
+package ru.gb;
+
+import java.util.logging.SocketHandler;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Василий", "Пупкин", 21);
+        Person person2 = new Person("Василий", "Пупкин", 21);
+        Person person3 = new Person("Иван", "Пупкин", 23);
+
+        //toString
+        System.out.println(person1);
+
+        //hashCode
+        System.out.println(person1.hashCode());
+
+        //equals
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.equals(person3));
+
+        //Сериализация
+        String toJson = Person.toJson(person1);
+        System.out.println(toJson);
+
+        //Десериализация
+        Person person4 = Person.fromJson(toJson);
+        System.out.println(person4);
+    }
+}
---
+++ HW/HW1/src/main/java/ru/gb/Person.java
@@ -0,0 +1,126 @@
+package ru.gb;
+
+
+import com.google.gson.Gson;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+
+public class Person implements Serializable {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    /**
+     * Реализован hashCode с помощью org.apache.commons.lang3.builder.hashCodeBuilder.
+     * И механизма рефлексии.
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+
+        HashCodeBuilder hashCodeBuilder = new HashCodeBuilder(17, 37);
+
+        Field[] fields = this.getClass().getDeclaredFields();
+
+        for (Field fl : fields) {
+            try {
+                hashCodeBuilder.append(fl.get(this));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return hashCodeBuilder.toHashCode();
+    }
+
+    /**
+     * Реализован equals с помощью org.apache.commons.lang3.builder.EqualsBuilder.
+     * И механизма рефлексии.
+     *
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+
+        Person rhs = (Person) obj;
+
+        EqualsBuilder equalsBuilder = new EqualsBuilder();
+
+        Field[] fields = this.getClass().getDeclaredFields();
+
+        for (Field fl : fields) {
+            try {
+                equalsBuilder.append(fl.get(this), fl.get(rhs));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return equalsBuilder.isEquals();
+    }
+
+    /**
+     * Реализован toString с помощью org.apache.commons.lang3.builder.ToStringBuilder.
+     * И механизма рефлексии.
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+
+        ToStringBuilder msg = new ToStringBuilder(this);
+        Field[] fields = this.getClass().getDeclaredFields();
+
+        for (Field fl : fields) {
+            try {
+                msg.append(fl.getName(), fl.get(this));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return msg.toString();
+    }
+
+    /**
+     * Сериализация с помощью Gson. Без обработки.
+     *
+     * @return - JSON String
+     */
+    public static String toJson(Person person) {
+        return new Gson().toJson(person);
+    }
+
+    /**
+     * Десериализация с помощью Gson. Без обработки.
+     *
+     * @param strJson -JSON String
+     * @return - Persone
+     */
+    public static Person fromJson(String strJson) {
+        return new Gson().fromJson(strJson, Person.class);
+    }
+
+
+
+
+
+}
